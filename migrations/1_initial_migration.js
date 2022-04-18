16ea7150165d73c0da331c5f5f52b662b1601a0a
chaiqing <chaiqing@gitlab.com>
2022-04-18 18:38:43 +0800

    fix：ci

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -7,7 +7,7 @@ const Durian = artifacts.require("./Durian.sol");
 
 const whiteAddress = ["0x8624b717379cb81954f0a1e8cbf22ea2966f0c64"];
 
-const executor = ["0x01", "0x02"];
+const executor = ["0x4aAbCC2aDcaC52706156Ed7BFEb0294A649F5684", "0xa36FA6F13FCB6a78945a8fF4fbBfABD9A1daa16c", "0x775FeD8E4E57a349e986e6b88E6b05eFFDbEB325", "0x437672Ee962971e3fDa4366334B680724Ac9d89E"];
 
 module.exports = async function (deployer) {
   await deployer.deploy(Durian, executor, whiteAddress);
